3e941c96fb7d0ee1621d7fc265d294d2c9e52a63
VINICIUS JOSE PEREIRA <162238067+vini-j-pereira@users.noreply.github.com>
2025-04-11 16:49:13 -0300

    init project

---
+++ DonateCrypto.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+
+struct Campaign {
+    address author;
+    string title;
+    string description;
+    string videoUrl;
+    string imageUrl;
+    uint256 balance;
+    bool active;
+}
+
+contract DonateCrypto {
+
+    uint256 public fee = 100; //wei
+    uint256 public nextId = 0;
+
+    mapping(uint256 => Campaign) public campaign; //id => campanha 
+
+    function addCampaign(string calldata title, string calldata description, string calldata videoUrl, string calldata imageUrl) public {
+        Campaign memory newCampaign;
+        newCampaign.title = title;
+        newCampaign.description = description;
+        newCampaign.videoUrl = videoUrl;
+        newCampaign.imageUrl = imageUrl;
+        newCampaign.active = true;
+        newCampaign.author = msg.sender;
+
+        nextId++;
+        campaign[nextId] = newCampaign;
+    }
+
+}
